24c49f6c9d97de05b459cc78bef55358ef1a7926
sumit1804 <sumit2002yadav2002@gmail.com>
2023-11-28 19:01:56 +0530

    updated server.ts

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import authRoutes from "./src/routes/auth";
 import eventRoutes from "./src/routes/event";
+import resourceRoutes from "./src/routes/resource";
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/event", eventRoutes);
+app.use("/api/resource", resourceRoutes )
 
 const PORT = 3001;
 app.listen(PORT, () => {
